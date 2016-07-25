f18f4e33d63a7aa939555ad8611115296e3a4f44
Sheesh Mohsin <sheeshmohsin@gmail.com>
2016-07-26 01:10:24 +0530

    Updated utils for handling values greater than 10**6

--- callhubapp/models.py
+++ callhubapp/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from callhubapp.utils import fib
+from callhubapp.utils import fib, matrix_pow, fib_matrix
 
 # Create your models here.
 class Fibonacci(models.Model):
@@ -17,4 +17,7 @@ class Fibonacci(models.Model):
 		return self.inp
 
 	def _get_fibonacci(self):
-		return fib(int(self.inp))
+		if int(self.inp) <= 10**6:
+			return fib(int(self.inp))
+		else:
+			return matrix_pow(fib_matrix, int(self.inp), 1000000007)[0][1]
--- callhubapp/utils.py
+++ callhubapp/utils.py
@@ -2,7 +2,37 @@
 '''
 Matrix based fibonacci finding function
 '''
+fib_matrix = [[1,1],
+              [1,0]]
 
+def matrix_square(A, mod):
+    return mat_mult(A,A,mod)
+
+
+def mat_mult(A,B, mod):
+  if mod is not None:
+    return [[(A[0][0]*B[0][0] + A[0][1]*B[1][0])%mod, (A[0][0]*B[0][1] + A[0][1]*B[1][1])%mod],
+            [(A[1][0]*B[0][0] + A[1][1]*B[1][0])%mod, (A[1][0]*B[0][1] + A[1][1]*B[1][1])%mod]]
+
+
+def matrix_pow(M, power, mod):
+    #Special definition for power=0:
+    if power <= 0:
+      return M
+    powers =  list(reversed([True if i=="1" else False for i in bin(power)[2:]])) #Order is 1,2,4,8,16,...
+    matrices = [None for _ in powers]
+    matrices[0] = M
+    for i in range(1,len(powers)):
+        matrices[i] = matrix_square(matrices[i-1], mod)
+    result = None
+    for matrix, power in zip(matrices, powers):
+        if power:
+            if result is None:
+                result = matrix
+            else:
+                result = mat_mult(result, matrix, mod)
+    return result
+    
 def pow(x, n, I, mult):
     """
     Returns x to the power of n. Assumes I to be identity relative to the
@@ -37,4 +67,5 @@ def matrix_multiply(A, B):
 def fib(n):
     F = pow([[1, 1], [1, 0]], n, identity_matrix(2), matrix_multiply)
     return F[0][1]
+
 
